3f9f07f51328b3c17a212b1d2607b9a7d434bd7f
Sergey Bogdanov <sergey.bogdanov@gmail.com>
2015-06-15 15:28:41 +0300

    - initial import

---
+++ BabelLibrary/HelloService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BabelLibrary
+{
+	internal class HelloService
+	{
+		internal HelloService ()
+		{
+		}
+
+		internal string Hello(string name)
+		{
+			return "Hello, " + name;
+		}
+	}
+}
+
